518dffaa91ada4a8ca51bd7bc45adf5f05a51c66
Ville Harvala <ville.harvala@mystes.fi>
2016-05-18 16:26:03 +0300

    Give credit to WSO2 in Java file headers

--- src/test/java/fi/mystes/synapse/mediator/xml/CustomCalloutMediatorFactoryTest.java
+++ src/test/java/fi/mystes/synapse/mediator/xml/CustomCalloutMediatorFactoryTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 Mystes Oy
+ * Copyright 2016: Originally made by WSO2, Inc. (http://wso2.com), Modified by Mystes Oy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
